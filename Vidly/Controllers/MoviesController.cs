d1946681d9b5b036bace6f860078fe4a4650954b
mateusz.malolepszy@gmail.com <mateusz.malolepszy@gmail.com>
2018-08-02 15:23:05 +0200

    Routes, Views are completed

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -12,13 +13,43 @@ namespace Vidly.Controllers
         // GET: Movies
         public ActionResult Random()
         {
-            var movie = new Movie(){Name = "Shrek!" };
-        
-//            return View(movie);
-//            return Content("Hello World");
-//            return HttpNotFound();
-//            return new EmptyResult();
-            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+            var movie = new Movie() {Name = "Shrek!"};
+            var customer = new List<Customer>
+            {
+                new Customer {Name = "Customer1"},
+                new Customer { Name = "Customer2" }
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customer
+            };
+
+            return View(viewModel);
         }
+//        public ActionResult Edit(int id)
+//        {
+//            return Content(("id=" + id));
+//        }
+//        // movies
+//        public ActionResult Index(int? pageIndex, string sortBy)
+//        {
+//            if (!pageIndex.HasValue)
+//            {
+//                pageIndex = 1;
+//            }
+//
+//            if (String.IsNullOrWhiteSpace(sortBy))
+//            {
+//                sortBy = "Name";
+//            }
+//
+//            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+//        }
+//        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+//        public ActionResult ByReleaseYear(int year, int month)
+//        {
+//            return Content(year + "/" + month);
+//        }
     }
 }
